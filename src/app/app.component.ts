56695316319ecf64b48d38c57c14d08c565498f8
Dealsa44 <z_tavartkiladze@cu.edu.ge>
2025-04-25 02:45:47 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pwa-install-demo';
+export class AppComponent implements OnInit {
+  title = 'PWA Install Demo';
+  showInstallButton = false;
+  deferredPrompt: any;
+
+  ngOnInit() {
+    console.log('Checking PWA installation status');
+    if (!this.isRunningStandalone()) {
+      console.log('Listening for beforeinstallprompt');
+      window.addEventListener(
+        'beforeinstallprompt',
+        this.onBeforeInstallPrompt
+      );
+      window.addEventListener('appinstalled', () => {
+        console.log('App installed successfully');
+        this.showInstallButton = false;
+      });
+    } else {
+      console.log('App is running in standalone mode');
+    }
+ 
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.addEventListener('controllerchange', () => {
+        window.location.reload();
+      });
+    }
+  }
+
+  private onBeforeInstallPrompt = (e: Event) => {
+    e.preventDefault();
+    this.deferredPrompt = e;
+    this.showInstallButton = true;
+  };
+
+  installPwa() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        this.deferredPrompt = null;
+        this.showInstallButton = false;
+      });
+    }
+  }
+
+  isRunningStandalone(): boolean {
+    return (
+      window.matchMedia('(display-mode: standalone)').matches ||
+      (window.navigator as any).standalone ||
+      document.referrer.includes('android-app://') ||
+      window.location.search.includes('mode=standalone')
+    );
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ]
 };
